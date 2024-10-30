dba91eaaa9709d41d591297fd899700e4602f5c3
Jack Kovryzhenko <yzk0058@auburn.edu>
2024-10-29 21:24:15 -0500

    solved last problem

--- hw04.m
+++ hw04.m
@@ -20,46 +20,81 @@ classdef hw04
                     D(j,k) = (D(j,k-1)-D(j-1,k-1)) / (data(j, 1) - data(j-k+1, 1));
                 end
             end
+            
             c = diag(D);
             n_evals = length(eval_);
-            y = zeros(1,n_evals);
-            y(:) = c(1);
-            for i_pts = 1:n_evals
-                for k = 2:n
-                    cumprod = c(k);
-                    for j = 1:k-1
-                        cumprod = cumprod*(eval_(i_pts) - data(j,1));
-                    end
-                    y(i_pts) = y(i_pts)+cumprod;
+            y = zeros(1, n_evals);
+            for k = 1:n_evals
+                result = c(end);
+                for i = n-1:-1:1
+                    result = result * (eval_(k) - data(i,1)) + c(i);
                 end
+                y(k) = result;
             end
         end
 
+    function y = p2(data, eval_)
+        % for 6630 ONLY
+        % Implement the divided difference method to interpolate the data 
+        % points, then evaluate the polynomial at the given point.
+        %
+        % :param data: a cell array of size n x 1, each cell is a vector 
+        %              like (x, y0, y1, y2, y3, ..., ym). 
+        %              y0 = f(x), 
+        %              y1 = f'(x), 
+        %              y2 = f''(x),
+        %              ... ,
+        %              ym = f^{(m)}(x).
+        % 
+        %              Note, different cells may have different lengths.
+        %
+        % :param eval: a vector of x values at which to evaluate the polynomial
+        % :return: a vector of y values corresponding to the evaluation points
 
-        function y = p2(data, eval)
-            % for 6630 ONLY
-            % Implement the divided difference method to interpolate the data 
-            % points, then evaluate the polynomial at the given point.
-            %
-            % :param data: a cell array of size n x 1, each cell is a vector 
-            %              like (x, y0, y1, y2, y3, ..., ym). 
-            %              y0 = f(x), 
-            %              y1 = f'(x), 
-            %              y2 = f''(x),
-            %              ... ,
-            %              ym = f^{(m)}(x).
-            % 
-            %              Note, different cells may have different lengths.
-            %
-            % :param eval: a vector of x values at which to evaluate the polynomial
-            % :return: a vector of y values corresponding to the evaluation points
-
+        n = sum(cellfun(@(x) length(x) - 1, data));
+        D = zeros(n);
+        derivs = cell(1,n);
+        
+        x = zeros(1,n);
+        idx = 0;
+        for i = 1:length(data)
+            idx = idx + 1;
+            x(idx) = data{i}(1);
+            D(idx,1) = data{i}(2);
             
-            n = length(data);
-            y = zeros(size(eval));
+            for ii = 3:length(data{i})
+                idx = idx + 1;
+                x(idx) = data{i}(1);
+                D(idx,1) = data{i}(2);
+                derivs{idx} = data{i}(3:end);
+            end
+        end
 
-            % write your code here.
-            
+        
+        for k = 2:n
+            for j = k:n
+                if x(j) == x(j-k+1)
+                    D(j,k) = derivs{j}(k-1)/factorial(k-1);
+                else
+                    D(j,k) = (D(j,k-1)-D(j-1,k-1)) / (x(j) - x(j-k+1));
+                end
+            end
+        end
+    
+        c = diag(D);    
+        n_evals = length(eval_);
+        y = zeros(1, n_evals);
+        for k = 1:n_evals
+            result = c(end);
+            for i = n-1:-1:1
+                result = result * (eval_(k) - x(i)) + c(i);
+            end
+            y(k) = result;
         end
     end
+
+
+
+    end
 end
+
--- hw_test.m
+++ hw_test.m
@@ -7,6 +7,14 @@ hw_assert(abs(hw04_worker.p1([-1, 1;1, 2;3, 7], 0) - 1)<1e-8);
 hw_assert(abs(hw04_worker.p1([-1, 1;1, 2;3, 7; 4, 2], 0) + 0.8)<1e-8);
 hw_assert(abs(hw04_worker.p1([-1, 1;1, 2;3, 7; 4, 2; 5, 9], 0) + 6.25)<1e-8);
 
+
+hw_assert(abs(hw04_worker.p2({[0, 0], [1,1,3,6]}, 0.5) - 1/8 ) < 1e-8);
+hw_assert(abs(hw04_worker.p2({[0, 0], [1,1,4,12,24]}, 0.5) - 1/16 ) < 1e-8);
+hw_assert((abs(hw04_worker.p2({[0, 0, 1], [1, 3, 6]}, [2, 3]) - [14, 39] ) < 1e-8) );
+
+
+
 function hw_assert(X)
     if X; fprintf('\t PASS\n'); else; fprintf('\t FAIL\n'); end
 end
+
